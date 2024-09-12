22e50987860d8efd7c4a78c1e6b4a69f3d1479c2
taha <gouyandeht@gmail.com>
2024-09-12 11:44:06 +0330

    initial project with next js

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <main className={""}>
+      </main>
+      <footer className={""}>
+      </footer>
+    </Fragment>
+  );
+}
